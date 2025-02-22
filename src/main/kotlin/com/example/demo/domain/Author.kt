f982c3634c8d771bcb00bf4d708ebce177ee4465
404 <sametalpdeger9@gmail.com>
2025-02-22 15:36:26 +0300

    first commit

---
+++ src/main/kotlin/com/example/demo/domain/Author.kt
@@ -0,0 +1,11 @@
+package com.example.demo.domain
+
+data class Author(
+    var id: Long?,
+    var name: String,
+    var age: Int,
+    var description: String,
+    var image: String
+) {
+
+}
---
+++ src/main/kotlin/com/example/demo/domain/Book.kt
@@ -0,0 +1,10 @@
+package com.example.demo.domain
+
+data class Book (
+    var isbn: String,
+    var title: String,
+    var description: String,
+    var image: String,
+    var author: Author,
+) {
+}
